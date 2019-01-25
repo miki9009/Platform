e7f3e4bff2f8f35b34d35fb6cd912959bae0555f
mikolaj <mikolaj.rambiert@gmail.com>
2019-01-25 17:34:53 +0100

    Props update

--- Assets/Scripts/Engine/Props/PropsPainter.cs
+++ Assets/Scripts/Engine/Props/PropsPainter.cs
@@ -15,7 +15,8 @@ namespace Engine.Props
         public bool continues = false;
         public float eraseRadius;
         public LayerMask collisionLayer;
-        public Vector3 camPos;
+        public Transform prefabContainer;
+
 
 
         public GameObject propPrefab;
@@ -34,20 +35,52 @@ namespace Engine.Props
         public float minScale;
         public float maxScale;
 
+        public bool useRandomPrefabs;
+        public List<GameObject> randomPrefabs;
+
+        [HideInInspector]
+        public Vector3 camPos;
+
         [HideInInspector]
         public Vector3 normal;
 
+        [HideInInspector]
         public GameObject target;
 
         Vector3 prevPos;
-
+        Bounds bounds;
+        GameObject prevPrefab;
+        Vector3 scale;
         private void OnDrawGizmos()
         {
+            Gizmos.color = Color.white;
             Gizmos.DrawWireSphere(camPos, 0.75f);
             Gizmos.color = Color.blue;
             Gizmos.DrawSphere(camPos, 0.25f);
             Gizmos.color = Color.red;
             Gizmos.DrawLine(camPos, camPos + normal * 5);
+            if(!erase)
+            {
+                if(prevPrefab != propPrefab)
+                {
+                    var mesh = propPrefab.GetComponentInChildren<MeshRenderer>();
+                    bounds = mesh.bounds;
+                    prevPrefab = propPrefab;
+                    if(mesh.transform.parent!=null)
+                    {
+                        scale = mesh.transform.lossyScale;
+                    }
+                    else
+                    {
+                        scale = mesh.transform.localScale;
+                    }
+
+
+                }
+                Gizmos.color = Color.green;
+                var s = new Vector3(scale.x * prefabScale.x, scale.y * prefabScale.y, scale.z * prefabScale.z);
+                Gizmos.DrawWireCube(camPos, new Vector3(bounds.size.x * s.x, bounds.size.y * s.y, bounds.size.z * s.z));
+            }
         }
 
         public void CreateProp()
@@ -72,10 +105,16 @@ namespace Engine.Props
                 var prop = Instantiate(propPrefab, camPos,  Quaternion.Euler(randomRotation ? 
                     new Vector3(Random.Range(xRot.x,xRot.y), Random.Range(yRot.x, yRot.y), Random.Range(zRot.x, zRot.y)): prefabRotation)).transform;
 
-                prop.localScale = randomScale ?  Vector3.one *  Random.Range(minScale, maxScale) : prefabScale;
-                prop.SetParent(transform);
+                prop.localScale = prefabScale;
+                prefabScale = randomScale ? Vector3.one * Random.Range(minScale, maxScale) : prefabScale;
+                prop.SetParent(prefabContainer ? prefabContainer : transform);
                 prop.gameObject.AddComponent<Prop>();
                 createdProps.Add(prop.gameObject);
+
+                if(useRandomPrefabs)
+                {
+                    propPrefab = randomPrefabs[Random.Range(0, randomPrefabs.Count)];
+                }
             }
         }
 
