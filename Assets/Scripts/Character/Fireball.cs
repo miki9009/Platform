c8d925009c9b2530ed4403b320fd37d2aaebf010
mikolaj <mikolaj.rambiert@gmail.com>
2019-01-31 18:05:53 +0100

    Fireball

---
+++ Assets/Scripts/Character/Fireball.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        StaticParticles.CreateExplosion(collision.contacts[0].point);
+        gameObject.SetActive(false);
+    }
+}
